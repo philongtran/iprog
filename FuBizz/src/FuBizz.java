f4f38465e7151378aad30ae597471edee2f50195
Manuel <wtfjoke@livenet.ch>
2015-12-14 11:05:09 +0100

    added javadoc

--- FuBizz/src/FuBizz.java
+++ FuBizz/src/FuBizz.java
@@ -1,6 +1,20 @@
-
+/**
+ * Solves the FuBizz Challenge
+ * 
+ * @author Manuel Wessner <191711>
+ * @author Phi Long Tran <191624>
+ * @author Steve Nono <191709>
+ */
 public class FuBizz {
 
+	/**
+	 * Replaces multiple of 3 with "Fizz" <br>
+	 * Replaces multiple of 5 with "Buzz" <br>
+	 * Replaces multiple of 3 and 5 with "FuBizz" <br>
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		int n = IO.readInt("n: ");
 
