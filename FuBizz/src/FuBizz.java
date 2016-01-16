d3c7080438ccac1def6d5f265c0865577809c232
Manuel <wtfjoke@livenet.ch>
2016-01-16 01:46:39 +0100

    remove useless stuff

--- FuBizz/src/FuBizz.java
+++ FuBizz/src/FuBizz.java
@@ -17,13 +17,12 @@ public class FuBizz {
 	 */
 	public static void main(String[] args) throws Exception {
 		int n = IO.readInt("n: ");
-
 		for (int i = 1; i <= n; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				IO.write("FuBizz, ");
-			} else if (i % 3 == 0 && i % 5 != 0) {
+			} else if (i % 3 == 0) {
 				IO.write("Fizz, ");
-			} else if (i % 5 == 0 && i % 3 != 0) {
+			} else if (i % 5 == 0) {
 				IO.write("Buzz, ");
 			} else {
 				IO.write(i + ", ");
